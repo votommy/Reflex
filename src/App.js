8e12696cc808a59344e36c534b9c92c06d813208
Tommy Vo <votommy@outlook.com>
2022-05-29 21:30:17 -0500

    HTTPS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
 const API_KEY = "da716740";
-const API_URL = "http://www.omdbapi.com?apikey=" + API_KEY;
+const API_URL = "https://www.omdbapi.com?apikey=" + API_KEY;
 
 const App = () => {
     const [movies, setMovies] = useState([]);
